efe6d33e8d22d340b5d2f11c3ca57a8d9d870892
j316chuck <j316chuck@berkeley.edu>
2018-03-09 15:26:48 -0800

    fixed caller bug that returned wrong df

--- caller.R
+++ caller.R
@@ -12,21 +12,20 @@ library(RcppArmadillo)
 # }
 
 sourceCpp("Rreconstruct2.cpp")
-test_link <- function(input_file_path) {
+reconstruct <- function(input_file_path) {
   df = read.table(input_file_path)
   df[, 1] = as.character(df[, 1])
   df[, 2] = sapply(df[, 2], as.character)
   result = reconstruct_caller(df[, 1], df[, 2], df[, 3])
-  return(df)
+  return(result)
 }
-
 
 main <- function() {
     cwd <- "~/Desktop/Research/Line/Rline"
     path <- "./test_cases/cases/test"
     setwd(cwd)
     for (i in 1:4) {
-      df = test_link(sprintf("%s%d.txt", path, i))
+      df = reconstruct(sprintf("%s%d.txt", path, i))
       file = sprintf("result%d.txt", i)
       for(i in 1:nrow(df)) {
         cat(sprintf("%s\t%s\t%f", df[i, 1], df[i, 2], df[i, 3]), file = file, append = TRUE, sep = '\n')
