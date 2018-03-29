e84728899f9158d40a1762b7236c9fcd081a7d32
j316chuck <j316chuck@berkeley.edu>
2018-03-28 22:21:51 -0700

    caller update with line

--- caller.R
+++ caller.R
@@ -8,8 +8,8 @@ reconstruct <- function(df, max_depth = 1, max_k = 0) {
   return(reconstruct_caller(as.character(df[, 1]), as.character(df[, 2]), as.numeric(df[, 3]), max_depth, max_k))
 }
 
-line <- function(df) {
-  line_caller()
+line <- function(df, binary = 0, dim = 100, order = 2, negative = 5, samples = 1, rho = 0.025, threads = 1) {
+  return(line_caller(as.character(df[, 1]), as.character(df[, 2]), as.numeric(df[, 3]), binary, dim, order, negative, samples, rho, threads))
 }
 
 normalize <- function(df) {
@@ -32,27 +32,36 @@ main <- function() {
     make_option(c("-d", "--max_depth"), type="integer", default=1, 
                 help="max_depth parameter", metavar="number"),
     make_option(c("-k", "--max_k"), type="integer", default=0, 
-                help="max_k parameter", metavar="number")
+                help="max_k parameter", metavar="number"),
+    make_option(c("-b", "--binary"), type="integer", default=0, 
+                help ="binary format?", metavar="number"),
+    make_option(c("-di", "--dim"), type="integer", default=100, 
+                help ="dimensions", metavar="number"),
+    make_option(c("-or", "--order"), type="integer", default=2, 
+                help ="order 1 or 2 in line", metavar="number"),
+    make_option(c("-n", "--negative"), type="integer", default=5, 
+                help ="number of negative samples", metavar="number"),
+    make_option(c("-s", "--samples"), type="integer", default=1, 
+                help ="number of total samples in million", metavar="number"),
+    make_option(c("-r", "--rho"), type="numeric", default=0.025, 
+                help ="rho value in line", metavar=NULL),
+       make_option(c("-t", "--threads"), type="integer", default=1, 
+                help ="number of threads used in line", metavar="number"),
   ); 
   opt_parser = OptionParser(option_list = option_list);
   options = parse_args(opt_parser);
   if (is.null(options$command)) {
       stop("Please enter a command")
-  } else if (options$command ==  "reconstruct") {
-    if (is.null(options$input_file)) {
+  } else if (is.null(options$input_file)) {
       stop("Please enter an input_file to read data from")
-    } else if (options$max_depth <= 0) {
+  } else if (options$command ==  "reconstruct") {
+    if (options$max_depth <= 0) {
       stop("Please enter a positive max_depth or else reconstruct won't work")
     }
     input_df = read.table(options$input_file)
     if (ncol(input_df) == 2) {
-	input_df$new_column <- rep(1, nrow(input_df))
+	    input_df$new_column <- rep(1, nrow(input_df))
     }
-    #fout <- file(options$output_file, "w")
-    #for(j in 1:nrow(input_df)) {
-    #   cat(sprintf("%s %s %f", input_df[j, 1], input_df[j, 2], input_df[j, 3]), file = fout, sep = '\n') 
-    #}
-    #close(fout)
     reconstruct_df = reconstruct(input_df, options$max_depth, options$max_k)
     fout <- file(options$output_file, "w")
     for(j in 1:nrow(reconstruct_df)) {
@@ -60,7 +69,17 @@ main <- function() {
     }
     close(fout)
   } else if (options$command == "line") {
-    warning("Not implemented yet")
+    input_df = read.table(options$input_file)
+    line_df = line_df(input_df, options$binary, options$dim, options$order, options$negative, options$samples, options$rho, options$threads)
+    fout <- file(options$output_file, "w")
+    for(j in 1:nrow(reconstruct_df)) {
+      cat(sprintf("%s ", reconstruct_df[j, 1]), file = fout)
+      for (k in 2:ncol(reconstruct_df)) {
+        cat(sprintf("%lf "), reconstruct_df[j, k], file = fout)
+      }
+      cat(sprintf("\n"), file = fout)
+    }
+    close(fout)
   } else if (options$command == "normalize") {
     warning("Not implemented yet")
   } else if (options$command == "concatenate") {
--- caller.cpp
+++ caller.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <stdio.h>
 #include "reconstruct_vector.h"
+#include "line_vector.h"
 
 // [[Rcpp::export]]
 Rcpp::DataFrame reconstruct_caller(Rcpp::StringVector input_u, Rcpp::StringVector input_v, Rcpp::NumericVector input_w, int max_depth = 1, int max_k = 0) {
@@ -23,6 +24,35 @@ Rcpp::DataFrame reconstruct_caller(Rcpp::StringVector input_u, Rcpp::StringVecto
 }
 
 
+
+// [[Rcpp::export]]
+Rcpp::DataFrame line_caller(Rcpp::StringVector input_u, Rcpp::StringVector input_v, Rcpp::NumericVector input_w, int binary = 0, int dim = 100, int order = 2, int negative = 5, int samples = 1, float rho = 0.025, int threads = 1) {
+  std::vector<std::string> iu(input_u.size()), iv(input_v.size()), output_vertices;
+  std::vector<double> iw(input_w.size());
+  std::vector< std::vector<double> > output_vectors;
+  for (int i = 0; i < input_u.size(); i++) {
+    iu[i] = (std::string) input_u(i);
+    iv[i] = (std::string) input_v(i);
+    iw[i] = (double) input_w(i);
+  }
+
+  TrainLINEMain(iu, iv, iw, output_vertices, output_vectors, binary, dim, order, negative, samples, rho, threads);
+  
+  long long row = (long long) output_vectors.size(), col = (long long) output_vectors[0].size();
+  Rcpp::StringVector output_v(row);
+  output_v = output_vertices;
+  Rcpp::DataFrame df = Rcpp::DataFrame::create(Rcpp::Named("vertice") = output_v);  
+  for (long long c = 0; c < col; c++) {
+      Rcpp::NumericVector vec(row);
+      for (long long r = 0; r < row; r++) {
+        vec[r] = output_vectors[r][c];
+      }
+      df.push_back(vec);
+  }
+  return df;
+}
+
+
 /*FILE *file = fopen("caller_result.txt", "w");
  printf("Debugging Result:\n");
  for (int i = 0; i < (int) ou.size(); i++) {
@@ -30,16 +60,3 @@ Rcpp::DataFrame reconstruct_caller(Rcpp::StringVector input_u, Rcpp::StringVecto
  }
  fclose(file);
 */
-
-
-// [[Rcpp::export]]
-void line_caller() {
-  printf("Calling Line\n");
-}
-
-
-
-// /*** R
-// input_df = read.table("./test_cases/cases/test1.txt")
-// reconstruct_caller(as.character(input_df[,1]), as.character(input_df[,2]), as.numeric(input_df[,3]), 2, 1000)
-// */
