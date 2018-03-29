d7472d8b93be69459f73ae9b6d843ff8ea3a90e1
j316chuck <j316chuck@berkeley.edu>
2018-03-29 00:11:49 -0700

    line vector

---
+++ line_vector.h
@@ -0,0 +1,9 @@
+#include <vector>
+#include <string>
+
+#ifndef LINE_H
+#define LINE_H
+
+void TrainLINEMain(const std::vector<std::string> &input_u, const std::vector<std::string> &input_v, const std::vector<double> &input_w, std::vector<std::string> &output_vertices, std::vector< std::vector<double> > &output_vectors, int is_binary_param, int dim_param, int order_param, int num_negative_param, int total_samples_param, float init_rho_param, int num_threads_param);
+#endif
+
